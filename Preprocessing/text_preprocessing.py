0b258dbd903f1deccaecbcb699d0ca2afc776f05
bzabk <bzabkowski@gmail.com>
2025-03-12 21:51:10 +0100

    add tsne and DBSCAN to textpipeline

--- Preprocessing/text_preprocessing.py
+++ Preprocessing/text_preprocessing.py
@@ -2,6 +2,8 @@ import string
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
 
 
 class RemovePunctuationTransformer(BaseEstimator, TransformerMixin):
@@ -24,12 +26,16 @@ class TextPipeline:
 
     def __init__(self):
         self.pipeline = Pipeline()
+        self.cluster = DBSCAN(eps=0.5, min_samples=10)
         self.punctuation_remover = RemovePunctuationTransformer()
         self.vectorizer = TfidfVectorizer(max_features=100, stop_words='english', min_df=2)
+        self.tsne = TSNE(n_components=3)
 
         self.pipeline = Pipeline([
             ('remove_punctuation', self.punctuation_remover),
-            ('vectorizer', self.vectorizer)
+            ('vectorizer', self.vectorizer),
+            ('clusterting',self.cluster),
+            ('dimensionality_reduction', self.tsne)
         ])
 
         self.is_fitted = False
@@ -39,9 +45,15 @@ class TextPipeline:
         self.is_fitted = True
 
     def transform(self, X):
-        if (self.is_fitted == False):
+        if self.is_fitted == False:
             raise ValueError("Pipeline not fitted")
         return self.pipeline.transform(X)
 
-class Manifolds:
-    pass
+    def get_cluster_labels(self):
+        if not self.is_fitted:
+            raise ValueError("Pipeline not fitted")
+        return self.cluster.labels_
+
+
+
+
