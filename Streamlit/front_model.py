00d16a35e34827bb7957e43474bef3db8d29241d
bzabk <bzabkowski@gmail.com>
2025-03-13 22:10:27 +0100

    MainApp class

--- Streamlit/front_model.py
+++ Streamlit/front_model.py
@@ -1,42 +1,48 @@
 import streamlit as st
-from typing import Text
+
+class MainApp:
 
 
-def main():
+    def __init__(self):
+        self.load_examplary_data_set = False
+        if "uploaded_file" not in st.session_state:
+            st.session_state.uploaded_file = None
+
+    def prepare_side_bar(self):
+        with st.sidebar:
+            st.sidebar.title("Enter txt file with list of PMIDs", anchor="center")
+            st.session_state.uploaded_file = st.file_uploader("Choose a file", type=["txt"], accept_multiple_files=False, label_visibility="collapsed")
+            if st.button("Analyse list of PMIDs", use_container_width=True):
+                pass
+            st.text("or choose prepared dataset")
+            if st.button("Load examplary dataset", use_container_width=True):
+                self.handle_load_examplary_data_set()
+
+    def handle_load_examplary_data_set(self):
+        self.load_examplary_data_set = True
+        st.session_state.uploaded_file = None
+
+    def prepare_main_window(self):
+        with st.container(key="app_title"):
+            st.title("PubTrends: Data Insights for Enhanced Paper Relevance")
 
 
-    with open("Static/style.css") as css:
-        st.html(f"<style>{css.read()}</style>")
-
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebar"] {
-                width: 350px; 
-                min-width: 350px; 
-                max-width: 350px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    with st.container(key="app_title"):
-        st.title("PubTrends: Data Insights for Enhanced Paper Relevance")
-
-    with st.sidebar:
-        st.sidebar.title("Enter txt file with list of PMIDs", anchor="center")
-        st.file_uploader("Choose a file", type=["txt"], accept_multiple_files=False, label_visibility="collapsed")
-        st.text("or choose examplary file")
-        if st.button("Load examplary dataset", use_container_width=True):
+    def prepare_tabs(self):
+        tab_visualization, tab_info = st.tabs(["Visualization", "Info"])
+        with tab_visualization:
+            pass
+        with tab_info:
             pass
 
+    def load_styles(self):
 
-
-
-
-
+        with open("Static/style.css") as css:
+            st.markdown(f"<style>{css.read()}</style>",unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    main()
+    app = MainApp()
+    app.load_styles()
+    app.prepare_main_window()
+    app.prepare_side_bar()
+    app.prepare_tabs()
 
