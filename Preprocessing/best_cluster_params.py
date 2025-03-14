28f120d2fb583860e9aa8a8c93572427c9697da9
bzabk <bzabkowski@gmail.com>
2025-03-14 15:28:51 +0100

    some changes in finding best KMeans params function

--- Preprocessing/best_cluster_params.py
+++ Preprocessing/best_cluster_params.py
@@ -2,23 +2,24 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 
-def sillhoute_method(kmax: int,X):
-    sil=[]
-    for i in range(1,kmax+1):
+def sillhoute_method(kmax: int, X):
+    sil = []
+    for i in range(2, kmax + 1):
         kmeans = KMeans(n_clusters=i)
         kmeans.fit(X)
         labels = kmeans.labels_
-        sil.append(silhouette_score(X, labels, metric = 'euclidean'))
-    plt.plot(range(1,kmax+1),sil)
+        sil.append(silhouette_score(X, labels, metric='euclidean'))
+    plt.plot(range(2, kmax + 1), sil)
+    plt.show()
 
-def inertia_method(kmax:int,X):
+def inertia_method(kmax: int, X):
     wcss = []
-    for i in range(1,kmax+1):
+    for i in range(2, kmax + 1):
         kmeans = KMeans(n_clusters=i)
         kmeans.fit(X)
-        labels = kmeans.labels_
         wcss.append(kmeans.inertia_)
-    plt.plot(range(1,kmax+1),wcss)
+    plt.plot(range(2, kmax + 1), wcss)
+    plt.show()
 
 
 
