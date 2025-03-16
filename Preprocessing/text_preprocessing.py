1eec342d3c836bfef2e3b0430e234a35e092a5b7
bzabk <bzabkowski@gmail.com>
2025-03-16 20:33:17 +0100

    3d_plot from tsne dimension reduction

--- Preprocessing/text_preprocessing.py
+++ Preprocessing/text_preprocessing.py
@@ -1,4 +1,5 @@
 import string
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
@@ -15,7 +16,10 @@ class RemovePunctuationTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        X_transformed = X.apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+        if isinstance(X, pd.Series):
+            X_transformed = X.apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+        else:
+            X_transformed = pd.Series(X).apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
         return X_transformed
 
     def fit_transform(self, X, y=None):
@@ -25,65 +29,57 @@ class RemovePunctuationTransformer(BaseEstimator, TransformerMixin):
 class TextPipeline:
 
     def __init__(self,n_clusters=8,max_features=100):
-        self.pipeline = Pipeline()
+        self.pipeline = Pipeline([])
         self.cluster = KMeans(n_clusters=n_clusters)
         self.punctuation_remover = RemovePunctuationTransformer()
         self.vectorizer = TfidfVectorizer(max_features=max_features, stop_words='english', min_df=2)
         self.tsne_reduction = TSNE(n_components=3)
 
-        self.text_pipeline = Pipeline([
+        self.text_processing_pipeline = Pipeline([
             ('remove_punctuation', self.punctuation_remover),
             ('vectorizer', self.vectorizer)
         ])
-        self.text_cluster_pipeline = Pipeline([
-            ('text_pipeline', self.text_pipeline),
-            ('cluster', self.cluster)
-        ])
-        self.tsne_pipeline = Pipeline([
-            ('dimensionality_reduction',self.text_pipeline),
-            ('tsne', self.tsne_reduction)
-        ])
 
-        self.is_fitted_text_pipeline = False
-        self.is_fitted_text_cluster_pipeline = False
-        self.is_fitted_tsne_pipeline = False
+        self.is_fitted_text_processing_pipeline = False
+        self.is_fitted_cluster = False
+        self.is_fitted_tsne = False
 
-    def fit_text_pipeline(self,X):
-        self.is_fitted_text_pipeline = True
-        self.text_pipeline.fit(X)
-        return self
-    def fit_text_cluster_pipeline(self,X):
-        self.is_fitted_text_cluster_pipeline = True
-        self.text_cluster_pipeline.fit(X)
-        return self
-    def fit_tsne_pipeline(self,X):
-        self.is_fitted_tsne_pipeline = True
-        self.tsne_pipeline.fit(X)
+    def fit_text_processing_pipeline(self,X):
+        self.is_fitted_text_processing_pipeline = True
+        self.text_processing_pipeline.fit(X)
         return self
 
-    def transform_text_pipeline(self,X):
-        if not self.is_fitted_text_pipeline:
+    def fit_kmeans(self,X):
+        self.cluster.fit(X)
+        return self
+
+    def fit_tsne(self,X):
+        self.tsne_reduction.fit(X)
+        return self
+
+    def transform_text_processing_pipeline(self,X):
+        if not self.is_fitted_text_processing_pipeline:
             raise ValueError("Pipeline not fitted")
-        return self.text_pipeline.transform(X)
+        return self.text_processing_pipeline.transform(X)
 
-    def transform_text_cluster_pipeline(self,X):
-        if not self.is_fitted_text_cluster_pipeline:
-            raise ValueError("Pipeline not fitted")
-        return self.text_cluster_pipeline.transform(X)
+    def transform_kmeans(self,X):
+        if not self.is_fitted_cluster:
+            raise ValueError("Cluster not fitted")
+        return self.cluster.transform(X)
 
-    def transform_tsne_pipeline(self,X):
-        if not self.is_fitted_tsne_pipeline:
-            raise ValueError("Pipeline not fitted")
-        return self.tsne_pipeline.transform(X)
+    def transform_tsne(self,X):
+        if not self.is_fitted_tsne:
+            raise ValueError("TSNE not fitted")
+        return self.tsne_reduction.transform(X)
 
-    def fit_transform_text_pipeline(self,X):
-        return self.fit_text_pipeline(X).transform_text_pipeline(X)
+    def fit_transform_text_processing_pipeline(self,X):
+        return self.text_processing_pipeline.fit_transform(X)
 
-    def fit_transform_text_cluster_pipeline(self,X):
-        return self.fit_text_cluster_pipeline(X).transform_text_cluster_pipeline(X)
+    def fit_transform_kmeans(self,X):
+        return self.cluster.fit_transform(X)
 
-    def fit_transform_tsne_pipeline(self,X):
-        return self.fit_tsne_pipeline(X).transform_tsne_pipeline(X)
+    def fit_transform_tsne(self,X):
+        return self.tsne_reduction.fit_transform(X)
 
 
 
--- Streamlit/front_model.py
+++ Streamlit/front_model.py
@@ -1,10 +1,25 @@
+import numpy as np
 import streamlit as st
-
+import pandas as pd
+import sys
+import os
+import plotly.express as px
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Preprocessing.text_preprocessing import TextPipeline
 class MainApp:
 
 
     def __init__(self):
+        self.prepared_pubmed_data = None
+
+
         self.load_examplary_data_set = False
+
+
+        self.current_X = None
+        self.current_labels = None
+
+        self.text_pipeline = TextPipeline(n_clusters=8,max_features=10)
         if "uploaded_file" not in st.session_state:
             st.session_state.uploaded_file = None
 
@@ -12,32 +27,91 @@ class MainApp:
         with st.sidebar:
             st.sidebar.title("Enter txt file with list of PMIDs", anchor="center")
             st.session_state.uploaded_file = st.file_uploader("Choose a file", type=["txt"], accept_multiple_files=False, label_visibility="collapsed")
-            if st.button("Analyse list of PMIDs", use_container_width=True):
-                pass
-            st.text("or choose prepared dataset")
-            if st.button("Load examplary dataset", use_container_width=True):
-                self.handle_load_examplary_data_set()
-
-    def handle_load_examplary_data_set(self):
-        self.load_examplary_data_set = True
-        st.session_state.uploaded_file = None
+            if st.session_state.uploaded_file is not None:
+                if st.button("Load PMIDS file", use_container_width=True):
+                    try:
+                        self.load_user_list_of_pmids()
+                    except:
+                        st.error("Invalid file format")
+            st.text("or choose a preloaded dataset")
+            if st.button("Load preloaded dataset", use_container_width=True):
+                self.handle_preloaded_data_set()
 
     def prepare_main_window(self):
         with st.container(key="app_title"):
             st.title("PubTrends: Data Insights for Enhanced Paper Relevance")
 
-
     def prepare_tabs(self):
         tab_visualization, tab_info = st.tabs(["Visualization", "Info"])
         with tab_visualization:
+            if self.load_examplary_data_set:
+                self.load_3d_plot()
             pass
         with tab_info:
             pass
 
-    def load_styles(self):
+    def load_pubmed_data(self):
+        csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'PubMedAPI', 'PubMed_data.csv'))
+        self.prepared_pubmed_data = pd.read_csv(csv_path)
 
-        with open("Static/style.css") as css:
+    def load_user_list_of_pmids(self):
+        uploaded_file = st.session_state.uploaded_file
+        try:
+            list_of_pmids = self.validate_chosen_file(uploaded_file)
+            return list_of_pmids
+        except:
+            raise ValueError("Invalid file format")
+
+    @staticmethod
+    def load_styles():
+        css_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Static', 'style.css'))
+        with open(css_path) as css:
             st.markdown(f"<style>{css.read()}</style>",unsafe_allow_html=True)
+
+    def preprocess_raw_text(self):
+        self.load_pubmed_data()
+        self.prepared_pubmed_data["Text"] = self.prepared_pubmed_data[["Title","Summary","Overall_design","Experiment_type","Organism"]].apply(lambda x: ' '.join(x),axis=1)
+
+        X = self.text_pipeline.fit_transform_text_processing_pipeline(self.prepared_pubmed_data["Text"]).toarray()
+
+        self.text_pipeline.fit_kmeans(X)
+        self.current_X = self.text_pipeline.fit_transform_tsne(X)
+        self.current_labels = self.text_pipeline.cluster.labels_.astype(str)
+
+    def validate_chosen_file(self,uploaded_file):
+        file_content = uploaded_file.read().decode("utf-8")
+        list_of_pmids = []
+        pmids = file_content.split("\n")
+        for line in pmids:
+            line = line.replace(" ", "")
+            if line.isdigit():
+                list_of_pmids.append(int(line))
+        print(list_of_pmids)
+        return list_of_pmids
+
+    def handle_preloaded_data_set(self):
+        self.load_examplary_data_set = True
+        st.session_state.uploaded_file = None
+        self.preprocess_raw_text()
+
+    def load_3d_plot(self):
+        fig = px.scatter_3d(
+            x = self.current_X[:,0],
+            y = self.current_X[:,1],
+            z = self.current_X[:,2],
+            color = self.current_labels,
+            size_max=3,
+            category_orders={"color": np.unique(self.current_labels)}
+        )
+        fig.update_layout(scene=dict(
+            xaxis_title='X',
+            yaxis_title='Y',
+            zaxis_title='Z'
+        ),width=900, height=600)
+
+        st.plotly_chart(fig)
+
+
 
 if __name__ == "__main__":
     app = MainApp()
