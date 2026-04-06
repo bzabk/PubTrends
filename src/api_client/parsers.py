a29941bd6842af5d37ad1530b5ef6ee9a7e4d37f
bzabk <bzabkowski@gmail.com>
2026-04-06 14:32:29 +0200

    refactor: streamline exception handling and introduce ConnectionManager for async requests

--- src/api_client/parsers.py
+++ src/api_client/parsers.py
@@ -3,22 +3,19 @@ import xmltodict
 from src.api_client.dtos import DatasetLinkDto, DatasetSummaryDto, OverallDesignDto
 from src.exceptions.api_client_exceptions import ParserError
 
-
-def parse_pmid_to_dbidx(response_result: dict[str, Any], pmid: int) -> DatasetLinkDto:
+def parse_pmid_to_dbidx(response_result: dict[str, Any],pmid: str) -> DatasetLinkDto:
     try:
         db_ids = response_result["linksets"][0]["linksetdbs"][0]["links"]
     except Exception as e:
-        raise ParserError(f"Failed to parse bdidx for pmid") from e
+        raise ParserError(f"Failed to parse dbidx for {pmid}") from e
 
     return DatasetLinkDto(pmid=pmid, db_ids=db_ids)
 
-
-def parse_dataset_summary(response_result: dict[str, Any], db_idx: int) -> DatasetSummaryDto:
+def parse_dataset_summary(response_result: dict[str, Any],db_idx: str) -> DatasetSummaryDto:
     try:
-        item = response_result["result"][str(db_idx)]
+        item = response_result["result"][db_idx]
     except Exception as e:
         raise ParserError(f"Failed to parse dataset summary for {db_idx}") from e
-
     return DatasetSummaryDto(
         db_idx=db_idx,
         title=item["title"],
@@ -28,14 +25,12 @@ def parse_dataset_summary(response_result: dict[str, Any], db_idx: int) -> Datas
         gse_code=item["accession"],
     )
 
-
 def parse_overall_design(xml_text: str, gse_code: str) -> OverallDesignDto:
     try:
         data = xmltodict.parse(xml_text)
         overall_design = data["MINiML"]["Series"].get("Overall-Design")
     except Exception as e:
         raise ParserError(f"Failed to parse XML response for {gse_code}") from e
-
     return OverallDesignDto(
         gse_code=gse_code,
         overall_design=overall_design,
