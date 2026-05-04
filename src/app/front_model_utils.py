b7ce9d3dc7d128e14023862b158b4e4707fa2fdf
bzabk <bzabkowski@gmail.com>
2026-05-04 12:25:49 +0200

    feat: add file validation function to check uploaded PMIDs and handle errors

--- src/app/front_model_utils.py
+++ src/app/front_model_utils.py
@@ -1,6 +1,32 @@
 import streamlit as st
 
 
+
+def validate_chosen_file(uploaded_file, min_len_pmid_list) -> list[str] | None:
+    """
+    Function checks whether the uploaded file is in the correct format and extracts PMIDs from it.
+    In case user uploaded less than 10 correct PMIDs, an error message is displayed.
+    If txt file contains less than 10 PMIDs, the error message is displayed.
+
+    :param uploaded_file: The file uploaded by the user.
+
+    :return list[int]: A list of unique PMIDs extracted from the file.
+    """
+    if uploaded_file is None:
+        raise PmidTxtFileIsNoneException
+    file_content = uploaded_file.read().decode("utf-8")
+    list_of_pmids = []
+    pmids = file_content.split("\n")
+    for line in pmids:
+        line = line.replace(" ", "").strip()
+        if line.isdigit():
+            list_of_pmids.append(str(line))
+    list_of_pmids = list(set(list_of_pmids))
+    if len(list_of_pmids) < min_len_pmid_list:
+        raise NotEnoughPmidsInTxtFileException
+    return list_of_pmids
+
+
 def reset_select_boxes() -> None:
     """
     After loading a new dataset and setting a new 3D plot,
